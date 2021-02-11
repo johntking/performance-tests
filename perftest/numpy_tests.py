41600df1724c60bf8eb3b99a58c76ce0da02fff1
John King <john.tilden.king@gmail.com>
2021-02-11 10:56:35 -0800

    tabs to spaces

--- perftest/numpy_tests.py
+++ perftest/numpy_tests.py
@@ -3,29 +3,27 @@ import numpy as np
 
 
 
-def basic_numpy_3d(N):
+def basic3d(N):
 
-	def inner():
-		X = np.random.randn(N**3)
-		yield 'randn'
-		X = X.reshape(N, N, N)
-		yield 'reshape'
-		m1 = X.min(axis = 2).max(axis = 1).min()
-		yield 'dim-m/m'
-		X = X / m1
-		yield 'divide'
-		X = X ** 3
-		yield 'power'
-		X = X[X > 1]
-		yield 'boolmask'
-		X = np.sort(X, axis = 0)
-		yield 'sort'
-		m2 = X[int(len(X) / 2)]
-		yield 'select'
+    def inner():
+        X = np.random.randn(N**3)
+        yield 'randn'
+        X = X.reshape(N, N, N)
+        yield 'reshape'
+        m1 = X.min(axis = 2).max(axis = 1).min()
+        yield 'dim-m/m'
+        X = X / m1
+        yield 'divide'
+        X = X ** 3
+        yield 'power'
+        X = X[X > 1]
+        yield 'boolmask'
+        X = np.sort(X, axis = 0)
+        yield 'sort'
+        m2 = X[int(len(X) / 2)]
+        yield 'select'
 
-	inner.__name__ = f'basic_numpy_3d({N}[ ** 3])'
-
-	return inner
+    return inner
 
 
 
--- perftest/pretty_printer.py
+++ perftest/pretty_printer.py
@@ -4,84 +4,83 @@ import logging
 import numpy as np
 
 
-logging.basicConfig(level = logging.INFO)
-
 
 def measure_execution_times(generator, trials):
 
-	durations = OrderedDict()
+    durations = OrderedDict()
 
-	for _ in range(trials):
+    for _ in range(trials):
 
-		t0 = datetime.now()
+        t0 = datetime.now()
 
-		for flag in generator():
-			t1 = datetime.now()
-			durations.setdefault(flag, []).append((t1 - t0).total_seconds())
-			t0 = datetime.now()
+        for flag in generator():
+            t1 = datetime.now()
+            durations.setdefault(flag, []).append((t1 - t0).total_seconds())
+            t0 = datetime.now()
 
-	if {len(times) for flag, times in durations.items()} != {trials}:
-		logging.warning('Irregular flag counts')
+    if {len(times) for flag, times in durations.items()} != {trials}:
+        logging.warning('Irregular flag counts')
 
-	return durations
+    return durations
 
 
 
 def format_time_column(times, sig = 4):
-	"""
-	Designing this for cases when the times should be between 1ms and ~3h.
-	Not expecting any perfectly round numbers (e.g., 10 seconds exactly)
-	"""
+    """
+    Designing this for cases when the times should be between 1ms and ~3h.
+    Not expecting any perfectly round numbers (e.g., 10 seconds exactly)
+    """
 
-	lo, hi = [
-		int(np.ceil(np.log(x) / np.log(10)))
-		for x in [min(times), max(times)]
-	]
+    lo, hi = [
+        int(np.ceil(np.log(x) / np.log(10)))
+        for x in [min(times), max(times)]
+    ]
 
-	if hi <= 1 and lo <= -1:
-		fmt = f'{{0:.{sig - lo + 4}}}ms'
-		factor = 1000
-	else:
-		fmt = f'{{0:.{sig - lo}}}s'
-		factor = 1
+    if hi <= 1 and lo <= -1:
+        fmt = f'{{0:.{sig - lo + 4}}}ms'
+        factor = 1000
+    else:
+        fmt = f'{{0:.{sig - lo}}}s'
+        factor = 1
 
-	str_times = [fmt.format(t * factor) for t in times]
+    str_times = [fmt.format(t * factor) for t in times]
 
-	if all([t.startswith('0') for t in str_times]):
-		str_times = [t.lstrip('0') for t in str_times]
+    if all([t.startswith('0') for t in str_times]):
+        str_times = [t.lstrip('0') for t in str_times]
 
-	return str_times
+    return str_times
 
 
 
 def pretty_print_times(
-		generators,
-		trials = 15,
-		print_indices = [0, 2, 7, 12, 14],
-		colsep = '  '
-	):
+        generator,
+        trials = 15,
+        #print_indices = [0, 2, 7, 12, 14],
+        print_indices = 'auto',
+        colsep = '  '
+    ):
 
-	for g in generators:
+    if print_indices == 'auto':
+        print_indices = sorted({0, trials // 2, trials - 1})
 
-		print(g.__name__)
-		lines = [['flag'] + [f'({i})' for i in print_indices]]
+    lines = [['flag'] + [f'({i})' for i in print_indices]]
 
-		for flag, durs in measure_execution_times(g, trials = trials).items():
-			durs.sort()
-			lines.append([flag] + [durs[i] for i in print_indices])
+    for flag, durs in measure_execution_times(generator, trials = trials).items():
+        durs.sort()
+        lines.append([flag] + [durs[i] for i in print_indices])
 
-		columns = []
+    columns = []
 
-		for j in range(len(lines[0])):
+    for j in range(len(lines[0])):
 
-			c0 = [l[j] for l in lines]
-			c1 = [c0[0]] + format_time_column(c0[1:]) if j else c0
-			maxlen = max([len(x) for x in c1])
-			c2 = [x.rjust(maxlen) for x in c1]
-			columns.append(c2)
+        c0 = [l[j] for l in lines]
+        c1 = [c0[0]] + format_time_column(c0[1:]) if j else c0
+        maxlen = max([len(x) for x in c1])
+        c2 = [x.rjust(maxlen) for x in c1]
+        columns.append(c2)
 
-		for line in zip(*columns):
-			print(colsep.join(line))
+    for line in zip(*columns):
+        print(colsep.join(line))
 
 
 
